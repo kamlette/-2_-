90e350c35367857bdb9bf0c1d1c18a2ddaf71978
kamlette <92592624+kamlette@users.noreply.github.com>
2022-05-25 13:55:31 +0500

    Add files via upload

---
+++ pipeline.cpp
@@ -0,0 +1,19 @@
+﻿#include "pipeline.h"
+
+
+const Matrix4f* Pipeline::GetTrans() //ãåíåðàöèÿ èòîãîâîé ìàòðèöû ïðåîáðàçîâàíèÿ îáúåêòîâ
+{
+    Matrix4f ScaleTrans, RotateTrans, TranslationTrans, CameraTranslationTrans, CameraRotateTrans, PersProjTrans;
+
+    ScaleTrans.InitScaleTransform(m_scale.x, m_scale.y, m_scale.z);
+    RotateTrans.InitRotateTransform(m_rotateInfo.x, m_rotateInfo.y, m_rotateInfo.z);
+    TranslationTrans.InitTranslationTransform(m_worldPos.x, m_worldPos.y, m_worldPos.z);
+    //Ïðåîáðàçîâàíèå êàìåðû: ñìåùåíèå íà îáðàòíûé âåêòîð ïîçèöèè
+    CameraTranslationTrans.InitTranslationTransform(-m_camera.Pos.x, -m_camera.Pos.y, -m_camera.Pos.z);
+    //Âðàùåíèå êàìåðû
+    CameraRotateTrans.InitCameraTransform(m_camera.Target, m_camera.Up);
+    PersProjTrans.InitPersProjTransform(m_persProj.FOV, m_persProj.Width, m_persProj.Height, m_persProj.zNear, m_persProj.zFar);
+    //Ñòàâèì ìàòðèöó ïåðñïåêòèâû ïåðâîé, ò.ê. âåêòîð ïîçèöèè óìíîæàåòñÿ ñ ïðàâîé ñòîðîíû îò èòîãîâîé ìàòðèöû
+    m_transformation = PersProjTrans * CameraRotateTrans * CameraTranslationTrans * TranslationTrans * RotateTrans * ScaleTrans;
+    return &m_transformation;
+}
